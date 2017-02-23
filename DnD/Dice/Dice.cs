a978c9cca16687c2145685934a26280aa96dab88
lordcheezus <migliacio@hotmail.com>
2017-02-23 15:16:07 -0800

    Dice working to satisfaction. Unit tested involving averages.

---
+++ DnD/Dice/Dice.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+namespace DnD.Dice
+{
+    /// <summary>
+    /// The Dice object is a static class which needs no constructor - it may be called at any time.
+    /// ex: int attackRoll = (Dice.D6 + Dice.D6) + 1;
+    /// </summary>
+    public static class Dice
+    {
+        /// <summary>
+        /// Random seeded engine internal to a Dice object.
+        /// </summary>
+        private static Random rng = new Random();
+
+        /// <summary>
+        /// Returns to the caller a randomly generated number ranging from 1-2.
+        /// </summary>
+        public static int D2 { get { return rng.Next(1, 3); } }
+
+        /// <summary>
+        /// Return to the caller a randomly generated number ranging from 1-3.
+        /// </summary>
+        public static int D3 { get { return rng.Next(1, 4); } }
+
+        /// <summary>
+        /// Returns to the caller a randomly generated number ranging from 1-4.
+        /// </summary>
+        public static int D4 { get { return rng.Next(1, 5); } }
+
+        /// <summary>
+        /// Returns to the caller a randomly generated number ranging from 1-6.
+        /// </summary>
+        public static int D6 { get { return rng.Next(1, 7); } }
+
+        /// <summary>
+        /// Returns to the caller a randomly generated number ranging from 1-8.
+        /// </summary>
+        public static int D8 { get { return rng.Next(1, 9); } }
+
+        /// <summary>
+        /// Returns to the caller a randomly generated number ranging from 1-10.
+        /// </summary>
+        public static int D10 { get { return rng.Next(1, 11); } }
+
+        /// <summary>
+        /// Returns to the caller a randomly generated number ranging from 1-12.
+        /// </summary>
+        public static int D12 { get { return rng.Next(1, 13); } }
+
+        /// <summary>
+        /// Returns to the caller a randomly generated number ranging from 1-20.
+        /// </summary>
+        public static int D20 { get { return rng.Next(1, 21); } }
+    }
+}
--- SolutionUnitTests/UnitTests.cs
+++ SolutionUnitTests/UnitTests.cs
@@ -1,4 +1,5 @@
 ﻿using DnD;
+using DnD.Dice;
 using DnD.Enums.Items;
 using DnD.Enums.Qualities;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
@@ -59,6 +60,41 @@ namespace SolutionUnitTests
 
                 pages.Add(spell, theQuality);
             }
+        }
+
+        /// <summary>
+        /// A unit test to collectively test averages of the static Dice classes collection.
+        /// </summary>
+        [TestMethod]
+        public void TestTheDice()
+        {
+            int d2Sum = 0, d3Sum = 0, d4Sum = 0, d6Sum = 0, d8Sum = 0, d10Sum = 0, d12Sum = 0, d20Sum = 0;
+            int d2Avg = 0, d3Avg = 0, d4Avg = 0, d6Avg = 0, d8Avg = 0, d10Avg = 0, d12Avg = 0, d20Avg = 0;
+            int numberRolled = 100000;
+
+            // test the average for D2 at 100000 rolls.
+            for (int i = 0; i < numberRolled; i++)
+            {
+                d2Sum += Dice.D2;
+                d3Sum += Dice.D3;
+                d4Sum += Dice.D4;
+                d6Sum += Dice.D6;
+                d8Sum += Dice.D8;
+                d10Sum += Dice.D10;
+                d12Sum += Dice.D12;
+                d20Sum += Dice.D20;
+            }
+
+            d2Avg =  d2Sum / numberRolled;
+            d3Avg = d3Sum / numberRolled;
+            d4Avg = d4Sum / numberRolled;
+            d6Avg = d6Sum / numberRolled;
+            d8Avg = d8Sum / numberRolled;
+            d10Avg = d10Sum / numberRolled;
+            d12Avg = d12Sum / numberRolled;
+            d20Avg = d20Sum / numberRolled;       
+            
+            // Conclusion: the test rolls averages appear to be consistently half of the value of the Dice roll which is acceptable.     
         }
     }
 }
