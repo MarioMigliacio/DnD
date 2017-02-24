7ebc44e06565fdbcd5e4b11181523685e72c994b
lordcheezus <migliacio@hotmail.com>
2017-02-23 23:26:38 -0800

    Environments Enum folder in solution. Under environments are Terrains, and DungeonRoom enums currently fully implemented.

---
+++ DnD/Enums/Environments/DungeonRooms.cs
@@ -0,0 +1,108 @@
+﻿namespace DnD.Enums.Environments
+{
+    /// <summary>
+    /// The various types of rooms that can be found while exploring a dungeon.
+    /// </summary>
+    public enum DungeonRoom
+    {
+        /// <summary>
+        /// Represents the Arena Dungeon room.
+        /// </summary>
+        Arena,
+
+        /// <summary>
+        /// Represents the Armory Dungeon room.
+        /// </summary>
+        Armory,
+
+        /// <summary>
+        /// Represents the Audience chamber Dungeon room.
+        /// </summary>
+        AudienceChamber,
+
+        /// <summary>
+        /// Represents the Burial site Dungeon room.
+        /// </summary>
+        BurialSite,
+
+        /// <summary>
+        /// Represents the Communication center Dungeon room.
+        /// </summary>
+        CommunicationCenter,
+
+        /// <summary>
+        /// Represents the Cookery Dungeon room.
+        /// </summary>
+        Cookery,
+
+        /// <summary>
+        /// Represents the Dumping grounds Dungeon room.
+        /// </summary>
+        DumpingGrounds,
+
+        /// <summary>
+        /// Represents the Guard post Dungeon room.
+        /// </summary>
+        GuardPost,
+
+        /// <summary>
+        /// Represents the Library Dungeon room.
+        /// </summary>
+        Library,
+
+        /// <summary>
+        /// Represents the Museum Dungeon room.
+        /// </summary>
+        Museum,
+
+        /// <summary>
+        /// Represents the Nursery Dungeon room.
+        /// </summary>
+        Nursery,
+
+        /// <summary>
+        /// Represents the Observatory Dungeon room.
+        /// </summary>
+        Observatory,
+
+        /// <summary>
+        /// Represents the Prison Dungeon room.
+        /// </summary>
+        Prison,
+
+        /// <summary>
+        /// Represents the Quarters Dungeon room.
+        /// </summary>
+        Quarters,
+
+        /// <summary>
+        /// Represents the Store room Dungeon room.
+        /// </summary>
+        StoreRoom,
+
+        /// <summary>
+        /// Represents the Temple Dungeon room.
+        /// </summary>
+        Temple,
+
+        /// <summary>
+        /// Represents the Training area Dungeon room.
+        /// </summary>
+        TrainingArea,
+
+        /// <summary>
+        /// Represents the Transportation Dungeon room.
+        /// </summary>
+        Transportation,
+
+        /// <summary>
+        /// Represents the Water supply Dungeon room.
+        /// </summary>
+        WaterSupply,
+
+        /// <summary>
+        /// Represents the workshop Dungeon room.
+        /// </summary>
+        Workshop
+    }
+}
---
+++ DnD/Enums/Environments/Terrains.cs
@@ -0,0 +1,143 @@
+﻿namespace DnD.Enums.Environments
+{
+    /// <summary>
+    /// The various types of Outdoor Terrains prevalent in the world.
+    /// </summary>
+    public enum Terrain
+    {
+        /// <summary>
+        /// Represents the Terrain type of Basin.
+        /// </summary>
+        Basin,
+
+        /// <summary>
+        /// Represents the Terrain type of Bluff.
+        /// </summary>
+        Bluff,
+
+        /// <summary>
+        /// Represents the Terrain type of Canyon.
+        /// </summary>
+        Canyon,
+
+        /// <summary>
+        /// Represents the Terrain type of Chasm.
+        /// </summary>
+        Chasm,
+
+        /// <summary>
+        /// Represents the Terrain type of Cliff.
+        /// </summary>
+        Cliff,
+
+        /// <summary>
+        /// Represents the Terrain type of Crater.
+        /// </summary>
+        Crater,
+
+        /// <summary>
+        /// Represents the Terrain type of Crevice.
+        /// </summary>
+        Crevice,
+
+        /// <summary>
+        /// Represents the Terrain type of Delta.
+        /// </summary>
+        Delta,
+
+        /// <summary>
+        /// Represents the Terrain type of Dune.
+        /// </summary>
+        Dune,
+
+        /// <summary>
+        /// Represents the Terrain type of Esker.
+        /// </summary>
+        Esker,
+
+        /// <summary>
+        /// Represents the Terrain type of Fissure.
+        /// </summary>
+        Fissure,
+
+        /// <summary>
+        /// Represents the Terrain type of Glacier.
+        /// </summary>
+        Glacier,
+
+        /// <summary>
+        /// Represents the Terrain type of Gorge.
+        /// </summary>
+        Gorge,
+
+        /// <summary>
+        /// Represents the Terrain type of Grove.
+        /// </summary>
+        Grove,
+
+        /// <summary>
+        /// Represents the Terrain type of Hill.
+        /// </summary>
+        Hill,
+
+        /// <summary>
+        /// Represents the Terrain type of Island.
+        /// </summary>
+        Island,
+
+        /// <summary>
+        /// Represents the Terrain type of Lake.
+        /// </summary>
+        Lake,
+
+        /// <summary>
+        /// Represents the Terrain type of Meadow.
+        /// </summary>
+        Meadow,
+
+        /// <summary>
+        /// Represents the Terrain type of Mesa.
+        /// </summary>
+        Mesa,
+
+        /// <summary>
+        /// Represents the Terrain type of Mound.
+        /// </summary>
+        Mound,
+
+        /// <summary>
+        /// Represents the Terrain type of Oasis.
+        /// </summary>
+        Oasis,
+
+        /// <summary>
+        /// Represents the Terrain type of Overlook.
+        /// </summary>
+        Overlook,
+
+        /// <summary>
+        /// Represents the Terrain type of Plateau.
+        /// </summary>
+        Plateau,
+
+        /// <summary>
+        /// Represents the Terrain type of Ravine.
+        /// </summary>
+        Ravine,
+
+        /// <summary>
+        /// Represents the Terrain type of Sinkhole.
+        /// </summary>
+        Sinkhole,
+
+        /// <summary>
+        /// Represents the Terrain type of Stream.
+        /// </summary>
+        Stream,
+
+        /// <summary>
+        /// Represents the Terrain type of Wetland.
+        /// </summary>
+        Wetland
+    }
+}
