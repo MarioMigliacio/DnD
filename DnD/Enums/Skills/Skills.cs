d2f3d7898dce07f1066bd7b229fdbf2362e03b18
lordcheezus <migliacio@hotmail.com>
2017-02-25 00:49:56 -0800

    New branch work on skills.

---
+++ DnD/Enums/Skills/Skills.cs
@@ -0,0 +1,10 @@
+﻿namespace DnD.Enums.Skills
+{
+    /// <summary>
+    /// TODO tomorrow, 2/26 - mario
+    /// </summary>
+    public enum Skills
+    {
+        // all the dnd skills pertinant to project.
+    }
+}
