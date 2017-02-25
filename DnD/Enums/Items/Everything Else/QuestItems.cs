9f35c63b65973fceb7f8b2afe97752f032f81c09
DarthOzy <nehemiahsalo@gmail.com>
2017-02-24 23:07:28 -0800

    Revert "List of Feats now accessible in Enums namespace. Set up a resx string file to have descriptions available for feats. For some reason you can't use UserStrings.FeatStrings. ____ in the Unit Tests file, but it works - trust me ;)"
    This reverts commit efbe72294cbb9bcbe7d19adefec175270975e5d6.

--- DnD/Enums/Items/Everything Else/QuestItems.cs
+++ DnD/Enums/Items/Everything Else/QuestItems.cs
@@ -5,6 +5,18 @@
     /// </summary>
     public enum QuestItem
     {
+        // This shit is gonna be insane TODO. Reminder.. fuck you. (me)
+
+        /// <summary>
+        /// Represents the quest item Robe of Fuck Off which makes all NPCs ignore you.
+        /// </summary>
+        RobeOfFuckOff,
+
+        /// <summary>
+        /// Represents the quest item Shoes of Speed which allows you to use the drug of choice, speed.
+        /// </summary>
+        ShoesOfSpeed,
+
         /// <summary>
         /// Represents the quest item Robe of Fuck Off which makes all NPCs ignore you.
         /// </summary>
