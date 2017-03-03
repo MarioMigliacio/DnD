82b1b0284cda15e5d33b2075d9c3613080a584bb
lordcheezus <migliacio@hotmail.com>
2017-03-02 22:38:39 -0800

    removed a few unnecessary using directives.

--- DnD/Classes/HeroFeats/FeatFactory.cs
+++ DnD/Classes/HeroFeats/FeatFactory.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using DnD.Enums.ClassFeats;
+﻿using DnD.Enums.ClassFeats;
 
 namespace DnD.Classes.HeroFeats
 {
