8fc09175dd8a4fa1b76a3ec598c2d7fd4bcfa146
lordcheezus <migliacio@hotmail.com>
2017-03-03 13:10:58 -0800

    made npc type enum

---
+++ DnD/Enums/NpcMerchants/NpcMerchant.cs
@@ -0,0 +1,150 @@
+﻿namespace DnD.Enums.NpcMerchants
+{
+    public enum NpcMerchant
+    {
+        /// <summary>
+        /// Represents an NPC of type Armorer.
+        /// </summary>
+        Armorer,
+
+        /// <summary>
+        /// Represents an NPC of type Auctioneer.
+        /// </summary>
+        Auctioneer,
+
+        /// <summary>
+        /// Represents an NPC of type Alchemist.
+        /// </summary>
+        Alchemist,
+
+        /// <summary>
+        /// Represents an NPC of type Assassin.
+        /// </summary>
+        Assassin,
+
+        /// <summary>
+        /// Represents an NPC of type Bank.
+        /// </summary>
+        Bank,
+
+        /// <summary>
+        /// Represents an NPC of type Baker.
+        /// </summary>
+        Baker,
+
+        /// <summary>
+        /// Represents an NPC of type Basketweaver.
+        /// </summary>
+        Basketweaver,
+
+        /// <summary>
+        /// Represents an NPC of type Brickmaker.
+        /// </summary>
+        Brickmaker,
+
+        /// <summary>
+        /// Represents an NPC of type Burglar.
+        /// </summary>
+        Burglar,
+
+        /// <summary>
+        /// Represents an NPC of type Blacksmith.
+        /// </summary>
+        Blacksmith,
+
+        /// <summary>
+        /// Represents an NPC of type Butcher.
+        /// </summary>
+        Butcher,
+
+        /// <summary>
+        /// Represents an NPC of type Bookseller.
+        /// </summary>
+        Bookseller,
+
+        /// <summary>
+        /// Represents an NPC of type Cartographer.
+        /// </summary>
+        Cartographer,
+
+        /// <summary>
+        /// Represents an NPC of type Goldsmith.
+        /// </summary>
+        Goldsmith,
+
+        /// <summary>
+        /// Represents an NPC of type Gambler.
+        /// </summary>
+        Gambler,
+
+        /// <summary>
+        /// Represents an NPC of type Jeweler.
+        /// </summary>
+        Jeweler,
+
+        /// <summary>
+        /// Represents an NPC of type Mason.
+        /// </summary>
+        Mason,
+
+        /// <summary>
+        /// Represents an NPC of type Messenger.
+        /// </summary>
+        Messenger,
+
+        /// <summary>
+        /// Represents an NPC of type Miller.
+        /// </summary>
+        Miller,
+
+        /// <summary>
+        /// Represents an NPC of type Pawnclerk.
+        /// </summary>
+        Pawnclerk,
+
+        /// <summary>
+        /// Represents an NPC of type Painter.
+        /// </summary>
+        Painter,
+
+        /// <summary>
+        /// Represents an NPC of type Provisioner.
+        /// </summary>
+        Provisioner,
+
+        /// <summary>
+        /// Represents an NPC of type Stable.
+        /// </summary>
+        Stable,
+
+        /// <summary>
+        /// Represents an NPC of type Sage.
+        /// </summary>
+        Sage,
+
+        /// <summary>
+        /// Represents an NPC of type Scribe.
+        /// </summary>
+        Scribe,
+
+        /// <summary>
+        /// Represents an NPC of type Spicemerchant.
+        /// </summary>
+        Spicemerchant,
+
+        /// <summary>
+        /// Represents an NPC of type Tavern.
+        /// </summary>
+        Tavern,
+
+        /// <summary>
+        /// Represents an NPC of type Tanner.
+        /// </summary>
+        Tanner,
+
+        /// <summary>
+        /// Represents an NPC of type Warehouse.
+        /// </summary>
+        Warehouse
+    }
+}
