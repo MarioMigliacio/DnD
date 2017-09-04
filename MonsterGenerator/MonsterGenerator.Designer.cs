4ca3ad03b653ff7e4147629cdd599e8bfd9b1b05
MarioMigliacio <migliacio@hotmail.com>
2017-09-03 17:58:02 -0700

    starting point for new work

---
+++ MonsterGenerator/MonsterGenerator.Designer.cs
@@ -0,0 +1,185 @@
+﻿namespace MonsterGenerator
+{
+    partial class MonsterGenerator
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.sortByChallengeRatingToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.challengeRatingToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.monsterTypeToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.monsterBiomeToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.alphabeticalToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.Preview = new System.Windows.Forms.GroupBox();
+            this.PreviewValue = new System.Windows.Forms.TextBox();
+            this.saveThisPreviewToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.AddButton = new System.Windows.Forms.Button();
+            this.RemoveButton = new System.Windows.Forms.Button();
+            this.menuStrip1.SuspendLayout();
+            this.Preview.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(12, 55);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(328, 21);
+            this.comboBox1.TabIndex = 0;
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.sortByChallengeRatingToolStripMenuItem,
+            this.saveThisPreviewToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(868, 24);
+            this.menuStrip1.TabIndex = 1;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // sortByChallengeRatingToolStripMenuItem
+            // 
+            this.sortByChallengeRatingToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.challengeRatingToolStripMenuItem,
+            this.monsterTypeToolStripMenuItem,
+            this.monsterBiomeToolStripMenuItem,
+            this.alphabeticalToolStripMenuItem});
+            this.sortByChallengeRatingToolStripMenuItem.Name = "sortByChallengeRatingToolStripMenuItem";
+            this.sortByChallengeRatingToolStripMenuItem.Size = new System.Drawing.Size(102, 20);
+            this.sortByChallengeRatingToolStripMenuItem.Text = "Sorting Options";
+            // 
+            // challengeRatingToolStripMenuItem
+            // 
+            this.challengeRatingToolStripMenuItem.Name = "challengeRatingToolStripMenuItem";
+            this.challengeRatingToolStripMenuItem.Size = new System.Drawing.Size(164, 22);
+            this.challengeRatingToolStripMenuItem.Text = "Challenge Rating";
+            // 
+            // monsterTypeToolStripMenuItem
+            // 
+            this.monsterTypeToolStripMenuItem.Name = "monsterTypeToolStripMenuItem";
+            this.monsterTypeToolStripMenuItem.Size = new System.Drawing.Size(164, 22);
+            this.monsterTypeToolStripMenuItem.Text = "Monster Type";
+            // 
+            // monsterBiomeToolStripMenuItem
+            // 
+            this.monsterBiomeToolStripMenuItem.Name = "monsterBiomeToolStripMenuItem";
+            this.monsterBiomeToolStripMenuItem.Size = new System.Drawing.Size(164, 22);
+            this.monsterBiomeToolStripMenuItem.Text = "Monster Biome";
+            // 
+            // alphabeticalToolStripMenuItem
+            // 
+            this.alphabeticalToolStripMenuItem.Name = "alphabeticalToolStripMenuItem";
+            this.alphabeticalToolStripMenuItem.Size = new System.Drawing.Size(164, 22);
+            this.alphabeticalToolStripMenuItem.Text = "Alphabetical";
+            // 
+            // Preview
+            // 
+            this.Preview.Controls.Add(this.PreviewValue);
+            this.Preview.Location = new System.Drawing.Point(466, 38);
+            this.Preview.Name = "Preview";
+            this.Preview.Size = new System.Drawing.Size(390, 462);
+            this.Preview.TabIndex = 2;
+            this.Preview.TabStop = false;
+            this.Preview.Text = "Preview";
+            // 
+            // PreviewValue
+            // 
+            this.PreviewValue.Location = new System.Drawing.Point(6, 19);
+            this.PreviewValue.Multiline = true;
+            this.PreviewValue.Name = "PreviewValue";
+            this.PreviewValue.ReadOnly = true;
+            this.PreviewValue.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.PreviewValue.Size = new System.Drawing.Size(377, 437);
+            this.PreviewValue.TabIndex = 0;
+            // 
+            // saveThisPreviewToolStripMenuItem
+            // 
+            this.saveThisPreviewToolStripMenuItem.Name = "saveThisPreviewToolStripMenuItem";
+            this.saveThisPreviewToolStripMenuItem.Size = new System.Drawing.Size(112, 20);
+            this.saveThisPreviewToolStripMenuItem.Text = "Save This Preview";
+            // 
+            // AddButton
+            // 
+            this.AddButton.Location = new System.Drawing.Point(347, 55);
+            this.AddButton.Name = "AddButton";
+            this.AddButton.Size = new System.Drawing.Size(113, 23);
+            this.AddButton.TabIndex = 3;
+            this.AddButton.Text = "Add To Preview";
+            this.AddButton.UseVisualStyleBackColor = true;
+            // 
+            // RemoveButton
+            // 
+            this.RemoveButton.Location = new System.Drawing.Point(347, 85);
+            this.RemoveButton.Name = "RemoveButton";
+            this.RemoveButton.Size = new System.Drawing.Size(113, 23);
+            this.RemoveButton.TabIndex = 5;
+            this.RemoveButton.Text = "Remove From Preview";
+            this.RemoveButton.UseVisualStyleBackColor = true;
+            // 
+            // MonsterGenerator
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(868, 512);
+            this.Controls.Add(this.RemoveButton);
+            this.Controls.Add(this.AddButton);
+            this.Controls.Add(this.Preview);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.MaximizeBox = false;
+            this.Name = "MonsterGenerator";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Monster Generator";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.Preview.ResumeLayout(false);
+            this.Preview.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem sortByChallengeRatingToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem challengeRatingToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem monsterTypeToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem monsterBiomeToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem alphabeticalToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem saveThisPreviewToolStripMenuItem;
+        private System.Windows.Forms.GroupBox Preview;
+        private System.Windows.Forms.TextBox PreviewValue;
+        private System.Windows.Forms.Button AddButton;
+        private System.Windows.Forms.Button RemoveButton;
+    }
+}
+
---
+++ MonsterGenerator/MonsterGenerator.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Forms;
+using Microsoft.VisualBasic.FileIO;
+using System.Data.SqlClient;
+
+namespace MonsterGenerator
+{
+    public partial class MonsterGenerator : Form
+    {
+        private string _connectionString = @"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Users\Mario\documents\visual studio 2015\Projects\DnD\MonsterGenerator\MonsterDb.mdf;Integrated Security=True";
+
+        public MonsterGenerator()
+        {
+            InitializeComponent();
+
+            try
+            {
+                List<string> nameField = new List<string>();
+                List<float> crField = new List<float>();
+                List<string> typeField = new List<string>();
+
+                using (TextFieldParser parser = new TextFieldParser(@"db_file.txt"))
+                {
+                    parser.TextFieldType = FieldType.Delimited;
+                    parser.SetDelimiters(",");
+
+                    while (!parser.EndOfData)
+                    {
+                        string[] line = parser.ReadFields();
+
+                        // the name field of the data.
+                        nameField.Add(line[0].Replace(" ", string.Empty));
+
+                        // the cr field of the data.
+                        float num = 0f;
+                        bool convertedNumber = float.TryParse(line[1], out num);
+
+                        if (convertedNumber)
+                        {
+                            crField.Add(num);
+                        }
+                        else
+                        {
+                            throw new Exception("Error converting the cr to a float.");
+                        }
+
+                        // the type field of the data.
+                        typeField.Add(line[2]);
+                    }
+
+                    // kill whatever is in the current Database.
+                    using (SqlConnection connection = new SqlConnection(_connectionString))
+                    {
+                        connection.Open();
+                        string queryString = $"TRUNCATE TABLE dbo.MonsterDb";
+                        SqlCommand command = new SqlCommand(queryString, connection);
+                        SqlDataReader reader = command.ExecuteReader();
+                        reader.Close();
+                    }
+
+                    // if the data in the file is in a valid state, add all the rows into the database.
+                    if (typeField.Count == crField.Count && typeField.Count == nameField.Count)
+                    {
+                        for (int i = 0; i < typeField.Count; i++)
+                        {
+                            using (SqlConnection connection = new SqlConnection(_connectionString))
+                            {
+                                connection.Open();
+                                string queryString = $"INSERT INTO MonsterDB VALUES('{nameField[i]}', {crField[i]}, '{typeField[i]}');";
+                                SqlCommand command = new SqlCommand(queryString, connection);
+                                SqlDataReader reader = command.ExecuteReader();
+                                reader.Close();
+                            }
+                        }
+
+                        // populate the textbox1 with the records in the database.
+                        using (SqlConnection connection = new SqlConnection(_connectionString))
+                        {
+                            connection.Open();
+                            string queryString = $"SELECT * FROM MonsterDB";
+                            SqlCommand command = new SqlCommand(queryString, connection);
+                            SqlDataReader reader = command.ExecuteReader();
+
+                            if (reader.HasRows)
+                            {
+                                StringBuilder boxContents = new StringBuilder();
+
+                                while (reader.Read())
+                                {
+                                    comboBox1.Items.Add($"{reader.GetSqlString(1).ToString()}, {reader.GetDouble(2).ToString()}, { reader.GetSqlString(3).ToString()}");
+                                }
+                            }
+
+                            reader.Close();
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+        }
+
+        // quick way to delete table: "TRUNCATE TABLE dbo.MonsterDb"
+        // view table: "SELECT * FROM dbo.MonsterDb"
+    }
+}
